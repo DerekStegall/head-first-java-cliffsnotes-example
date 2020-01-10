a28d28ddea35bb037ab8db6e0e3bd363666e567a
Derek Stegall <dstegall@basecampcodingacademy.org>
2020-01-10 09:04:30 -0600

    CH3 Code snippet

---
+++ chapter-3/Chapter-3-CodeSnipp/src/app/MyArray.java
@@ -0,0 +1,17 @@
+package app;
+
+public class MyArray {
+
+    public static void main(String[] args) throws Exception {
+
+        Vehicle[] airplanes = new Vehicle[3]; // Creating Vehicle array variable
+
+        // Indexing to make each of the array items a Vehicle object
+
+        airplanes[0] = new Vehicle();
+
+        airplanes[1] = new Vehicle();
+
+        airplanes[2] = new Vehicle();
+    }
+}
