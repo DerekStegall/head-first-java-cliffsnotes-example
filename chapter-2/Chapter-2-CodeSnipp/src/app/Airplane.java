18fcf978fdf531690b62462625a02d702f26bb0e
Derek Stegall <dstegall@basecampcodingacademy.org>
2020-01-10 08:13:48 -0600

    code revisions for ch 2

--- chapter-2/Chapter-2-CodeSnipp/src/app/Airplane.java
+++ chapter-2/Chapter-2-CodeSnipp/src/app/Airplane.java
@@ -3,17 +3,13 @@ package app;
 // The inherting Subclass
 class Airplane extends Vehicle {
 
-    // Overriding an inherited method
-    public void honkhorn() {
-        System.out.println(horn);
-    }
-
     public static void main(String[] args) {
         Vehicle test = new Vehicle();
         test.type = "Airplane";
         test.year = "2009";
         test.horn = "This function is not available, while in the air";
 
+        // Overridng the method honkhorn()
         test.honkhorn();
 
     }
