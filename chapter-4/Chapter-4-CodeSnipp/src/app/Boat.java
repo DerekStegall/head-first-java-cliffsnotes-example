7eea0977aa1cd3dd917b015389d1ee1481c22c81
Derek Stegall <dstegall@basecampcodingacademy.org>
2020-01-29 09:28:03 -0600

    chapter-4-codesnipp

---
+++ chapter-4/Chapter-4-CodeSnipp/src/app/Boat.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Boat {
+
+    String ownerName;
+
+    public String getownerName() {
+
+        return ownerName;
+    }
+
+    public void setownerName(String name) {
+
+        ownerName = name;
+    }
+
+}
---
+++ chapter-4/Chapter-4-CodeSnipp/src/app/Example.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.util.Scanner;
+
+class Example {
+    public static void main(String[] args) {
+
+        Boat myBoat = new Boat();
+
+        myBoat.setownerName("Shrek");
+
+        System.out.println("Hello " + myBoat.getownerName() + " you are welcome");
+
+    }
+}
